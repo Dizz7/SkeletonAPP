c0bf59fdb68abbf2bd8a92fb84f9b68212580fb8
Dizz7 <162482518+Dizz7@users.noreply.github.com>
2025-06-01 13:13:18 -0400

    Primer Commit S2

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,87 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController, AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: false
+})
+export class LoginPage {
+
+  email: string = '';
+  password: string = '';
+  mensaje: string = '';
+
+  constructor(
+    private router: Router,
+    private toastController: ToastController,
+    private alertController: AlertController
+  ) {}
+
+  // Mostrar toast de inicio de sesión
+  async mostrarToasts() {
+    const toast = await this.toastController.create({
+      message: 'Iniciando Sesión.',
+      duration: 2000,
+      color: 'success'
+    });
+    await toast.present();
+  }
+
+  // Mostrar errores con alert
+  async mostrarError(mensaje: string) {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: mensaje,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  // Validar formato del email
+  validarEmail(email: string): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
+  // Método login con validaciones
+  async login() {
+    if (!this.email) {
+      this.mostrarError('Ingrese un e-mail.');
+      return;
+    }
+
+    if (this.email.length < 3) {
+      this.mostrarError('El email debe tener al menos 3 caracteres.');
+      return;
+    }
+
+    if (!this.validarEmail(this.email)) {
+      this.mostrarError('El email ingresado no es válido.');
+      return;
+    }
+
+    if (!this.password) {
+      this.mostrarError('Ingrese la contraseña.');
+      return;
+    }
+
+
+
+    if (this.password.length !== 4) {
+      this.mostrarError('La contraseña debe tener exactamente 4 dígitos.');
+      return;
+    }
+
+    if (!/^\d+$/.test(this.password)) {
+      this.mostrarError('La contraseña sólo puede contener dígitos.');
+      return;
+    }
+
+    // Si todo es correcto
+    await this.mostrarToasts();
+    this.router.navigate(['/home'], { state: { user: this.email } });
+  }
+}
